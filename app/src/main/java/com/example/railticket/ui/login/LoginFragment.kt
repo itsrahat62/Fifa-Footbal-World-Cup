731a420749a4e90047ad39ab65dcbc00d8556742
Rokon -4993 <PC01@DESKTOP-EJFFJ9F>
2025-10-13 16:51:18 +0600

    token collected

--- app/src/main/java/com/example/railticket/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/railticket/ui/login/LoginFragment.kt
@@ -1,9 +1,13 @@
 package com.example.railticket.ui.login
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.railticket.databinding.FragmentLoginBinding
@@ -12,6 +16,15 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private var authToken: String? = null
+
+    inner class WebAppInterface(private val context: Context) {
+        @JavascriptInterface
+        fun postToken(token: String) {
+            Log.d("LoginFragment", "Token from WebView: $token")
+            authToken = token
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,9 +39,58 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val webView = binding.webview
-        webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+        context?.let {
+            webView.addJavascriptInterface(WebAppInterface(it), "Android")
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                val script = """
+                    (function() {
+                        // Intercept fetch
+                        const originalFetch = window.fetch;
+                        window.fetch = function(url, options) {
+                            if (typeof url === 'string' && url.includes('https://railspaapi.shohoz.com/v1.0/web/auth/sign-in')) {
+                                return originalFetch.apply(this, arguments).then(response => {
+                                    if (response.ok) {
+                                        response.clone().json().then(data => {
+                                            if (data && data.data && data.data.token) {
+                                                Android.postToken(data.data.token);
+                                            }
+                                        });
+                                    }
+                                    return response;
+                                });
+                            }
+                            return originalFetch.apply(this, arguments);
+                        };
+
+                        // Intercept XMLHttpRequest
+                        const originalXHRSend = XMLHttpRequest.prototype.send;
+                        XMLHttpRequest.prototype.send = function(data) {
+                            this.addEventListener('load', function() {
+                                if (this.responseURL.includes('https://railspaapi.shohoz.com/v1.0/web/auth/sign-in')) {
+                                    try {
+                                        const responseData = JSON.parse(this.responseText);
+                                        if (responseData && responseData.data && responseData.data.token) {
+                                            Android.postToken(responseData.data.token);
+                                        }
+                                    } catch (e) {
+                                        // Not a JSON response or parsing error, ignore.
+                                        console.error('Error parsing response from sign-in API', e);
+                                    }
+                                }
+                            });
+                            originalXHRSend.apply(this, arguments);
+                        };
+                    })();
+                """
+                view?.evaluateJavascript(script, null)
+            }
+        }
         webView.loadUrl("https://eticket.railway.gov.bd/login")
     }
 
